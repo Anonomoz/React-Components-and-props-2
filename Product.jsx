06d1ad09ad115c33533aae4424b5d560349dcb78
Ömer <omer96@live.dk>
2025-04-20 17:07:01 +0200

    Update Product.jsx

--- Product.jsx
+++ Product.jsx
@@ -1,10 +1,10 @@
 export default function Product(props) {
-    const formattedPrice = props.price/100
+	const price = (props.price / 100).toFixed(2);
     return (
         <div className="product">
             <img className="product-image" width="272" height="300" alt={props.name} src={props.image} />
             <p className="product-name">{props.name}</p>
-            <div className="product-price">{'$'+formattedPrice.toFixed(2)}</div>
+            <div className="product-price">{'$'+price}</div>
         </div>
     );
 }
